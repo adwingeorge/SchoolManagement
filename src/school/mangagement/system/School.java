89d2e2caa6f7bc2726e7ed41c58e86ebfcb04514
adwin <adwin.george27@gmail.com>
2023-04-03 22:36:27 +0100

    simple school management

--- src/school/mangagement/system/School.java
+++ src/school/mangagement/system/School.java
@@ -1,10 +1,25 @@
 package school.mangagement.system;
 
+import java.util.List;
+
+/**
+ * many teachers, many students
+ * implements teachers and students using an arraylist
+ */
+
 public class School {
-    private  Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers;
+    private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
 
-
+    /**
+     * new school object is created
+     * @param teachers list of teachers
+     * @param students list of students
+     */
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+    }
 }
