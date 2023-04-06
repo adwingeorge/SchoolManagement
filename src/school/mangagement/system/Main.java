2e5d559deb238c14e1809762a68ca659b24004f5
adwin <adwin.george27@gmail.com>
2023-04-06 23:08:47 +0100

    simpleschoolmanagement

--- src/school/mangagement/system/Main.java
+++ src/school/mangagement/system/Main.java
@@ -2,7 +2,8 @@ package school.mangagement.system;
 
 public class Main {
     public static void main(String[] args) {
-        Teacher Lizzy = new Teacher(1, "Lizzy",500)
+        Teacher Lizzy = new Teacher(1, "Lizzy",500);
+        Teacher Arlin = new Teacher(1,"Arlin",700);
 
 
     }
